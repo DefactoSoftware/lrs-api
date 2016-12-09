5b7a92aa3bef1a2c413d0345320d226f868dde5c
Jesse Dijkstra <mail@jessedijkstra.nl>
2016-12-09 11:13:57 +0100

    Cast exp int to string for header

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -11,17 +11,17 @@ defmodule LrsApi.AuthController do
       {:ok, user} ->
         new_conn = Guardian.Plug.api_sign_in(conn, user)
         jwt = Guardian.Plug.current_token(new_conn)
-        claims = Guardian.Plug.claims(new_conn)
+        { :ok, claims } = Guardian.Plug.claims(new_conn)
         exp = Map.get(claims, "exp")
 
         new_conn
         |> put_resp_header("authorization", "Bearer #{jwt}")
-        |> put_resp_header("x-expires", exp)
+        |> put_resp_header("x-expires", "#{exp}")
         |> render("login.json", %{user: user, jwt: jwt, exp: exp})
       {:error, _changeset} ->
         conn
         |> put_status(401)
-        |> render "error.json", message: "Could not login"
+        |> render("error.json", message: "Could not login")
     end
   end
 end
